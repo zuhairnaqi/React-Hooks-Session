97baa059afd3950c19c31aca21ab9f239ef706d1
Zuhair Naqi <zuhair.naqi@iomechs.com>
2020-08-10 01:19:16 +0500

    Updated code

--- src/Components/Counter1.js
+++ src/Components/Counter1.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 
 function Counter1() {
     const [count, setCount] = useState(0);
+
+    const increment = () => setCount(prev => prev + 1);
+    const decrement = () => setCount(prev => prev - 1);
+
     return (
         <div>
-            <p>Count = {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <p>Counter I = {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
-
 export default Counter1;
--- src/Components/Counter2.js
+++ src/Components/Counter2.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react'
 
 function Counter2() {
     const [count, setCount] = useState(0);
+
+    const increment = () => setCount(prev => prev + 1);
+    const decrement = () => setCount(prev => prev - 1);
+
     return (
         <div>
-            <p>Count = {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <p>Counter II = {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
