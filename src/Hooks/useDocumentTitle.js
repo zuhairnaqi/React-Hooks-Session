492630a23e8d29d9fd649f3ccda9a980726ce705
Zuhair Naqi <zuhair.naqi@iomechs.com>
2020-08-10 01:02:12 +0500

    Deleted unwanted file

--- src/Hooks/useDocumentTitle.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react'
-
-function useDocumentTitle(count) {
-    useEffect(() => {
-        document.title = `Count is ${count}`;
-    }, [count])
-}
-
-export default useDocumentTitle;
